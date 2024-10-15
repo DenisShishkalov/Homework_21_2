f95d02b2576b27a3573a7295991b4b402c0bd1b6
Denis <denver.1299@mail.ru>
2024-10-15 09:32:09 +0300

    1 fixes config

--- src/config.py
+++ src/config.py
@@ -4,6 +4,3 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 DATA_DIR = BASE_DIR / 'html'
 
 file_contact_html = DATA_DIR / 'contact.html'
-file_transaction_csv = DATA_DIR / 'transactions.csv'
-file_transaction_json = DATA_DIR / 'transactions.json'
-file_json_product = DATA_DIR / 'products.json'
